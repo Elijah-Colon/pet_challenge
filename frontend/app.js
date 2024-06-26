6d5890a7ad48437f073eafd4071890b3c84338e5
Bryan Lopez Rosales <d00442163@CMP-SCC112-1113.local>
2024-06-26 11:12:25 -0600

    app

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,28 @@
+const URL = "http://localhost:8080";
+
 Vue.createApp({
-  data() {
-    return {};
-  },
-  methods: {},
-  created: function () {
-    console.log("app loaded");
-  },
-}).mount("#miau");
+    data() {
+      return {
+        pets: "",
+        locate:""
+      };
+    },
+    methods: { 
+      getPets: async function() {
+        let response = await fetch (`${URL}${locate}`);
+        let data = await response.json();
+        this.pets = data 
+      },
+      changePage: function () {
+        if (this.locate == '/pets'){
+        this.locate = '/applications'
+      }else{
+        this.locate = "/pets"
+      }
+    },
+
+    },
+    created: function () {
+      console.log("app loaded");
+    },
+  }).mount("#app");
