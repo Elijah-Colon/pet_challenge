1cd7711a840591b6642f144ce581cf277b63b791
Bryan Lopez Rosales <d00442163@CMP-SCC112-1113.local>
2024-06-26 12:09:46 -0600

    more get

--- frontend/app.js
+++ frontend/app.js
@@ -11,6 +11,7 @@ Vue.createApp({
     },
     methods: { 
       getPets: async function() {
+        this.fullPage = `${URL}` + this.locate
         let response = await fetch (this.fullPage);
         let data = await response.json();
         this.pets = data 
@@ -20,17 +21,18 @@ Vue.createApp({
         this.page = 1
         this.locate = '/pets'
         this.fullPage = `${URL}` + this.locate
-        console.log(this.fullPage)
+        console.log(this.pets)
       }else{
         this.page = 2
         this.locate = '/applications'
         this.fullPage = `${URL}` + this.locate
-        console.log(this.fullPage)
+        console.log(this.pets)
       }
     },
 
     },
     created: function () {
       console.log("app loaded");
+      this.getPets()
     },
   }).mount("#app");
